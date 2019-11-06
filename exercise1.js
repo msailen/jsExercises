928248f304259a1f185c9a2c4ab0e660af0a1435
Rumsan <work@MacBook-Pro.local>
2019-11-06 14:11:52 +0545

    exercise 1 modified

--- exercise1.js
+++ exercise1.js
@@ -2,13 +2,20 @@ var remainder, reverse;
 var reverse= 0;
 
 function reverseNumber(num){
-    while(num>0){
-        remainder= num%10;
-        reverse=(reverse*10)+remainder;
-        num=(num/10)|0;
+    var regex=/^[0-9]+$/;
+    if(regex.test(num)){
+        while(num>0){
+            remainder= num%10;
+            reverse=(reverse*10)+remainder;
+            num=(num/10)|0;
+        } 
+    console.log(reverse);
+    }else{
+        alert("Please enter a valid number");
     }
-    return reverse;
 }
-console.log(reverseNumber(32243));
+var userNum=prompt("Enter a number to reverse:");
+reverseNumber(userNum);
+
 
 
